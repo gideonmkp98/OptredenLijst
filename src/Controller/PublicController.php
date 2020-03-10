d334f7e352808a3f75c8c339a8838a29f1e1cdad
Gideon <info@gideonmkp.nl>
2020-03-10 08:52:53 +0100

    Push

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Optreden;
+use App\Form\ContactType;
 
 class PublicController extends AbstractController
 {
@@ -17,13 +18,26 @@ class PublicController extends AbstractController
             'controller_name' => 'PublicController',
         ]);
     }
+    /**
+     * @Route("/history", name="history")
+     */
     public function history(){
-     return true;
+        return true;
     }
     public function komendeOptredens(){
         return true;
     }
     public function weekOptredens(){
         return true;
+    }
+    /**
+     * @Route("/contact", name="controler")
+     */
+    public function contact(){
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('public/contact.html.twig', [
+                      'our_form' => $form->createView(),
+        ]);
     }
 }
